493eaccf405544013e49fc2ba90dc521b1493da7
Astha Jain <jainastha894@gmail.com>
2025-07-26 21:08:24 +0530

    updated dbport

--- db.js
+++ db.js
@@ -10,7 +10,7 @@ const db = new Pool({
   host: process.env.HOST,
   database: process.env.DATABASE,
   password: process.env.PASSWORD,
-  port: process.env.PORT,
+  port: process.env.DBPORT,
 });
 
 
