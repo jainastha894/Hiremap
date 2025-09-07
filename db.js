6fa66d1ed9784f942a0892398f8686cf583128f9
Astha Jain <jainastha894@gmail.com>
2025-09-07 12:43:50 +0530

    update database source

--- db.js
+++ db.js
@@ -5,12 +5,7 @@ env.config();
 const {Pool}=pkg;
 
 const db = new Pool({
-
-  user: process.env.USER,
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.DBPORT,
+connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
